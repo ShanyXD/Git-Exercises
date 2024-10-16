73e9c88e719e4a387a655aeab85bf297252ea0b7
Toshko06 <teorzivanov@gmail.com>
2024-10-16 11:08:44 +0300

    Hello

--- OptionsManage.cs
+++ OptionsManage.cs
@@ -8,5 +8,18 @@ namespace Calculator
 {
     internal class OptionsManage
     {
+        public static string[] OptionsList =
+        {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+            "dr - Divide Remainder",
+            "ex - Exit"
+        };
+        public static void DivideRemainder(double a, double b)
+        {
+            Console.WriteLine($"{a} % {b} = {a % b}");
+        }
+
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+              switch (choice)
+            {
+                case "a":
+                    OptionsManager.Add(a, b);
+                    break;
+                case "s":
+                    OptionsManager.Subtract(a, b);
+                    break;
+                case "m":
+                    OptionsManager.Multiply(a, b);
+                    break;
+                case "dr":
+                    OptionsManager.DivideRemainder(a, b);
+                    break;
+                case "ex":
+                    Console.Clear();
+                    Console.WriteLine("Goodbye");
+                    Console.Readkey(true);
+                    return;
+            }
+            Console.WritLine("Press any key to continue...");
+            Console.Readkey(true);
         }
     }
 }
